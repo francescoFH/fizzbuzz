b0b6a601e07e63ebd7c9a5d224d708675890223c
Francesco <francescofacca@gmail.com>
2020-10-20 13:03:45 +0100

    Add final test and program

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,26 +1,26 @@
 require "fizzbuzz"
-describe "fizzbuizz" do
+describe "fizzbuzz" do
   it "returns 'fizz' for the number 3" do
     expect(3.fizzbuzz).to eq "fizz"
   end
 end
 
 require "fizzbuzz"
-describe "fizzbuizz" do
+describe "fizzbuzz" do
   it "returns 'buzz' for the number 5" do
-    expect(5.fizzbuzz).to eq "fizz"
+    expect(5.fizzbuzz).to eq "buzz"
   end
 end
 
 require "fizzbuzz"
-describe "fizzbuizz" do
+describe "fizzbuzz" do
   it "returns 'fizzbuzz' for the number 15" do
-    expect(15.fizzbuzz).to eq "fizz"
+    expect(15.fizzbuzz).to eq "fizzbuzz"
   end
 end
 
 require "fizzbuzz"
-describe "fizzbuizz" do
+describe "fizzbuzz" do
   it "returns number for the number not divisible by 3 or 5" do
     expect(4.fizzbuzz).to eq 4
   end
